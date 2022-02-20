9b39c9b59bba7aacac7c16f63c98b296e6ffc470
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-02-20 22:38:50 +0900

    fix: jwtに関する修正

--- app/client/firebase_auth_client.rb
+++ app/client/firebase_auth_client.rb
@@ -1,6 +1,8 @@
-class FirebaseAuthClient
+module FirebaseAuthClient
+  ALGORITHM = 'RS256'.freeze
+
   class << self
-    def authenticate_user(token)
+    def verify_id_token(token)
       raise 'Id token must be a String' unless token.is_a?(String)
 
       full_decoded_token = decode_jwt(token, false)
@@ -16,16 +18,15 @@ class FirebaseAuthClient
       end
 
       certificate = OpenSSL::X509::Certificate.new(public_key)
-      decoded_token = decode_jwt(token, true, { algorithm: 'RS256', verify_iat: true }, certificate.public_key)
+      decoded_token = decode_jwt(token, true, certificate.public_key, { algorithm: ALGORITHM, verify_iat: true })
 
       User.find_or_create_by!(uid: decoded_token[:payload]['sub'], email: decoded_token[:payload]['email'])
     end
 
-
     # JWTをデコード
     def decode_jwt(token, key=nil, verify=false, options={})
       begin
-        decoded_token = JWT.decode(token, key, verify, options)
+        decoded_token = JWT.decode(token, verify, key, options)
       rescue JWT::ExpiredSignature => e
         raise 'Firebase ID token has expired. Get a fresh token from your client app and try again.'
       rescue => e
@@ -37,7 +38,6 @@ class FirebaseAuthClient
         header: decoded_token[1]
       }
     end
-
 
     # トークン情報のフォーマットチェック
     def validate(json)
@@ -66,7 +66,6 @@ class FirebaseAuthClient
       errors
     end
 
-
     # トークンが正しい鍵から作られているか確認する
     def fetch_public_keys
       # uri = URI.parse('https://www.googleapis.com/robot/v1/metadata/x509/securetoken@system.gserviceaccount.com')
@@ -84,12 +83,10 @@ class FirebaseAuthClient
       raise msg
     end
 
-
     def cached_connection
       Faraday.new do |builder|
         builder.use :http_cache, store: Rails.cache, logger: Rails.logger
         builder.request :url_encoded
-        builder.response :json, parser_options: { symbolize_names: true }, content_type: 'application/json'
         builder.adapter Faraday.default_adapter
       end
     end
