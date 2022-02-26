b3bfc74be374fbd6fb903f98e8fde6916036cd5f
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-02-27 00:07:36 +0900

    feat: product model

--- app/models/creator.rb
+++ app/models/creator.rb
@@ -2,4 +2,5 @@ class Creator < ApplicationRecord
   belongs_to :user
   has_one :creator_info
   has_many :creator_category
+  has_many :product
 end
---
+++ db/migrate/20220226145457_create_products.rb
@@ -0,0 +1,8 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      t.references :creator, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
