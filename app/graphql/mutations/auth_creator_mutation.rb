f1d47db1df16fc1b0ffbcdcbdba731dab09f52dc
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-01 21:46:21 +0900

    fix: args default params

--- app/graphql/mutations/auth_creator_mutation.rb
+++ app/graphql/mutations/auth_creator_mutation.rb
@@ -1,6 +1,6 @@
 module Mutations
   class AuthCreatorMutation < BaseMutation
-    def authorized?(_args)
+    def authorized?(_args = {})
       # 認可できない場合はGraphQL::ExecutionErrorをraise
       raise GraphQL::ExecutionError, 'login required!!' unless current_creator
 
