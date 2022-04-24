e8bd4da5cec4198133de8a5cfd80c1457c922394
hbk2002320@gmail.com <hbk2002320@gmail.com>
2022-04-25 00:16:47 +0900

    feat: order price and fee methood

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,16 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def price
+    product.product_info.price * quantity
+  end
+
+  def fee
+    application_fee * quantity
+  end
+
+  def application_fee
+    product.product_info.price * 0.1
+  end
 end
